2603fce615831fb6b9a50b3d4f15d2f289817d4a
Kwang Chin Lee <kwang@incube8.sg>
2016-07-17 17:08:02 +0800

    update forms

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -3,9 +3,7 @@
 @section('content')
     @include('shared.alert')
     <h1>Articles</h1>
-    {!! link_to_route('articles.create', 'New Article', null, [
-        'class' => 'btn btn-primary btn-lg',
-        'data-remote' => 'true' ]) !!}
+    {!! link_to_route('articles.create', 'New Article', null, ['class' => 'btn btn-primary btn-lg']) !!}
     <table class="table">
         <tr>
             <th>Edit</th>
