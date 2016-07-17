131ed56a797cbbb0823a1661710c7de1a4c0e2ed
Kwang Chin Lee <kwang@incube8.sg>
2016-07-17 17:40:04 +0800

    fix acceptance test

--- tests/acceptance/ArticleCest.php
+++ tests/acceptance/ArticleCest.php
@@ -72,7 +72,7 @@ class ArticleCest
     private function createAnArticle(AcceptanceTester $I, $attributes)
     {
         $I->haveAuthor();
-        $I->click('[href*=create]');
+        $I->click('//a[text()='New Article']');
         $this->submitTheForm($I, $attributes);
     }
 
